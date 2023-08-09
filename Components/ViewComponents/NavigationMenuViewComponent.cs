bf928102eda0e96160f70192ecc7deabbe81582f
Anton Korchynskyi <200524341@student.georgianc.on.ca>
2023-08-09 18:54:40 -0400

    Created Navigation Menu and updated the main Views

---
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoundHouseFun.Models;
+
+namespace SoundHouseFun.Components.ViewComponents
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var menuItems = new List<MenuItem>
+        {
+            new MenuItem { Controller = "Home", Action = "Index", Label = "Home" },
+            new MenuItem { Controller = "Shop", Action = "Index", Label = "Shop" },
+            new MenuItem { Controller = "Shop", Action = "ViewMyCart", Label = "Cart", Authorized = true },
+            new MenuItem { Controller = "Shop", Action = "Orders", Label = "My Orders", Authorized = true },
+            new MenuItem { Controller = "Orders", Action = "Orders", Label = "Admin", Authorized = true, AllowedRoles = new List<string> { "Administrator" }, DropdownItems = new List<MenuItem> {
+                new MenuItem { Controller = "Orders", Action = "Index", Label = "Orders" },
+                new MenuItem { Controller = "Carts", Action = "Index", Label = "Carts" },
+                new MenuItem { Controller = "Departments", Action = "Index", Label = "Departments" },
+                new MenuItem { Controller = "Products", Action = "Index", Label = "Products" },
+            }},
+            new MenuItem { Controller = "Home", Action = "About", Label = "About" },
+            new MenuItem { Controller = "Home", Action = "Contact", Label = "Contact" },
+            new MenuItem { Controller = "Home", Action = "Privacy", Label = "Privacy" },
+        };
+
+            return View(menuItems);
+        }
+    }
+}
