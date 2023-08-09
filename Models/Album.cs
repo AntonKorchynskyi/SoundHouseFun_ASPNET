fd2a67b7d953b41ab69a4ef7271e7205103da38c
Anton Korchynskyi <200524341@student.georgianc.on.ca>
2023-08-09 16:01:54 -0400

    Created Main Model Classes

---
+++ Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoundHouseFun.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "You must provide an Album Name"), MaxLength(50)]
+        public string Name { get; set; }
+
+        public string? Photo { get; set; }
+
+        public List<Song>? Songs { get; set; }
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoundHouseFun.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Album")]
+        public int AlbumId { get; set; }
+
+        [Required()]
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        [Required()]
+        public string Singer { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public Album? Album { get; set; }
+
+    }
+}
